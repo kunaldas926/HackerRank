1e22672d4e3fcc5a890926871e5f4035aaee6d94
Kunal Das <kunaldas926@yahoo.in>
2022-01-09 23:19:58 +0530

    cpp-hello-world

---
+++ cpp-hello-world/Solution.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//
+//int main() {
+//    std::cout << "Hello, World!" << std::endl;
+//    return 0;
+//}
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
